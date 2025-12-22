9b00c981f7a3a002c9027758ca166fff85d2e951
Rasoul <139282615+Rasoul01@users.noreply.github.com>
2025-12-23 02:55:21 +0330

    added project files

---
+++ src/main/java/com/gymconnect/GymconnectApplication.java
@@ -0,0 +1,13 @@
+package com.gymconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymconnectApplication.class, args);
+	}
+
+}
